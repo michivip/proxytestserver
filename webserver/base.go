4f723e896dad94157bd198fb50f3bb01aad28049
MichiVIP <michi.developer@gmail.com>
2018-01-02 01:18:24 +0100

    Add webserver directory with basic webserver construct

---
+++ webserver/base.go
@@ -0,0 +1,21 @@
+package webserver
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"log"
+)
+
+func StartWebserver(addr string) *http.Server {
+	router := mux.NewRouter()
+	server := &http.Server{
+		Handler: router,
+		Addr:    addr,
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Panic(err)
+		}
+	}()
+	return server
+}
