482734a823f99cbbfee57ee4a0b16edb26a629de
MichiVIP <michi.developer@gmail.com>
2018-01-04 03:50:33 +0100

    Outsource json write method and add proxycheck endpoint

--- webserver/endpoints.go
+++ webserver/endpoints.go
@@ -2,16 +2,89 @@ package webserver
 
 import (
 	"net/http"
-	"encoding/json"
+	"net"
+	"strings"
 	"log"
 )
 
-func EndpointRequestHeaders(writer http.ResponseWriter, req *http.Request) {
-	if err := json.NewEncoder(writer).Encode(req.Header); err != nil {
-		http.Error(writer, "500 Internal error", http.StatusInternalServerError)
-		log.Printf("Could not write headers to %v because of %T: %v", req.RemoteAddr, err, err)
-	} else {
-		writer.Header().Set("Content-Type", "application/json")
-		writer.WriteHeader(http.StatusOK)
+var proxyHeaders = []string{
+	//  proxy variables
+	"Via", "X_forwarded", "X_forwarded_for", "X-Forwarded-For", "X-Forwarded",
+	// http variables
+	"Http_forwarded", "Http-Forwarded", "Http_x_forwarded_for", "Http_client_ip", "Http_via", "Http_proxy_connection", "Http_proxy_connection", "Http-X-Forwarded-For", "Http-Client-Ip",
+}
+
+type CheckProxyResponse struct {
+	IsProxy              bool        `json:"is_proxy"`
+	ProxyHeaders         http.Header `json:"proxy_headers"`
+	SuspectedRealAddress string      `json:"suspected_real_address"`
+}
+
+func EndpointCheckProxy(writer http.ResponseWriter, req *http.Request) {
+	response := &CheckProxyResponse{
+		IsProxy:      false,
+		ProxyHeaders: http.Header{},
 	}
+	suspectedAddresses := fetchSuspectedIPAddresses(req, response)
+	remoteIpString := req.RemoteAddr[:strings.LastIndex(req.RemoteAddr, ":")]
+	var maximum int
+	for address, count := range suspectedAddresses {
+		if maximum < *count && address != remoteIpString {
+			response.SuspectedRealAddress = address
+		}
+	}
+	if response.SuspectedRealAddress == "" {
+		response.SuspectedRealAddress = remoteIpString
+	}
+	writeJsonResponse(writer, req, response)
+}
+
+func fetchSuspectedIPAddresses(req *http.Request, response *CheckProxyResponse) (suspectedAddresses map[string]*int) {
+	log.Printf("Proxy check from: %v\n", req.Header)
+	suspectedAddresses = make(map[string]*int, 0)
+	for _, proxyHeader := range proxyHeaders {
+		if value, ok := req.Header[proxyHeader]; ok {
+			response.ProxyHeaders[proxyHeader] = req.Header[proxyHeader]
+			if !response.IsProxy {
+				response.IsProxy = true
+			}
+			if len(value) == 1 {
+				if ip := parseIp(value[0]); ip != "" {
+					if value, ok := suspectedAddresses[ip]; ok {
+						*value += 1
+					} else {
+						initialValue := 1
+						suspectedAddresses[ip] = &initialValue
+					}
+				}
+			}
+		}
+	}
+	return
+}
+
+func parseIp(input string) (ip string) {
+	if ipBytes := net.ParseIP(input); ipBytes != nil {
+		return ipBytes.String()
+	} else if addresses, err := net.LookupHost(input); err != nil && len(addresses) > 0 {
+		return addresses[0]
+	} else if host, _, err := net.SplitHostPort(input); err == nil {
+		if addresses, err := net.LookupHost(host); err == nil && len(addresses) > 0 {
+			return addresses[0]
+		} else if ipBytes := net.ParseIP(input); ipBytes != nil {
+			return ipBytes.String()
+		}
+	} else {
+		split := strings.Split(input, " ")
+		for _, elem := range split {
+			if ip = parseIp(elem); ip != "" {
+				return ip
+			}
+		}
+	}
+	return
+}
+
+func EndpointRequestHeaders(writer http.ResponseWriter, req *http.Request) {
+	writeJsonResponse(writer, req, req.Header)
 }
