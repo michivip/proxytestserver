de2999ed45d87c350821d90148daeaafaa90e271
MichiVIP <michi.developer@gmail.com>
2018-01-05 17:50:30 +0100

    Improve scanning process

--- webserver/endpoints.go
+++ webserver/endpoints.go
@@ -46,12 +46,14 @@ func fetchSuspectedIPAddresses(req *http.Request, response *CheckProxyResponse) 
 				response.IsProxy = true
 			}
 			if len(value) == 1 {
-				if ip := parseIp(value[0]); ip != "" {
-					if value, ok := suspectedAddresses[ip]; ok {
-						*value += 1
-					} else {
-						initialValue := 1
-						suspectedAddresses[ip] = &initialValue
+				if ips := parseIps(value[0]); len(ips) > 0 {
+					for _, ip := range ips {
+						if value, ok := suspectedAddresses[ip]; ok {
+							*value += 1
+						} else {
+							initialValue := 1
+							suspectedAddresses[ip] = &initialValue
+						}
 					}
 				}
 			}
@@ -60,19 +62,17 @@ func fetchSuspectedIPAddresses(req *http.Request, response *CheckProxyResponse) 
 	return
 }
 
-func parseIp(input string) (ip string) {
+func parseIps(input string) (ip []string) {
 	if addresses, err := net.LookupHost(input); err != nil && len(addresses) > 0 {
-		return addresses[0]
+		return addresses
 	} else if host, _, err := net.SplitHostPort(input); err == nil {
 		if addresses, err := net.LookupHost(host); err == nil && len(addresses) > 0 {
-			return addresses[0]
-		} else if ipBytes := net.ParseIP(input); ipBytes != nil {
-			return ipBytes.String()
+			return addresses
 		}
 	} else {
 		split := strings.Split(input, " ")
 		for _, elem := range split {
-			if ip = parseIp(elem); ip != "" {
+			if ip = parseIps(elem); len(ip) != 0 {
 				return ip
 			}
 		}
